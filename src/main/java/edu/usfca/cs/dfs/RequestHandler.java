95a9682c08157562a643d79961f2d3b8a81dec1d
brianisadog <s254901282002@gmail.com>
2018-09-15 23:20:57 -0700

    Read from DatagramPacket

--- src/main/java/edu/usfca/cs/dfs/RequestHandler.java
+++ src/main/java/edu/usfca/cs/dfs/RequestHandler.java
@@ -1,5 +1,7 @@
 package edu.usfca.cs.dfs;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.DatagramPacket;
 
 class RequestHandler implements Runnable {
@@ -11,6 +13,15 @@ class RequestHandler implements Runnable {
 
     @Override
     public void run() {
+        byte[] receivedData = this.packet.getData();
 
+        try (ByteArrayInputStream inStream = new ByteArrayInputStream(receivedData)) {
+            StorageMessages.Info info = StorageMessages.Info.parseDelimitedFrom(inStream);
+            System.out.println(info.getType().toString());
+            System.out.println(info.getData().toStringUtf8());
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
